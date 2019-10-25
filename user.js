119f0c22af318afb65dd44db93d26ac7727534c2
garganshul108 <sci.agarg@gmail.com>
2019-10-25 14:44:19 +0530

    user movement set

--- user.js
+++ user.js
@@ -19,8 +19,14 @@ function User(x, y, dir, options) {
     this.color = options.color ? options.color : defaultColor;
 
 
+    this.modDir = () => {
+        this.dir = this.dir - (floor(this.dir / TWO_PI) * TWO_PI);
+    }
+
+
     this.rotate = (v) => {
         this.dir += v;
+        this.modDir();
     }
 
     this.rotateRight = () => {
@@ -29,6 +35,44 @@ function User(x, y, dir, options) {
 
     this.rotateLeft = () => {
         this.rotate(qR);
+    }
+
+    this.move = (vx, vy) => {
+        this.x += vx;
+        this.v += vy;
+    }
+
+    this.moveForward = () => {
+        let dx = cos(this.dir);
+        let dy = sin(this.dir);
+        if (abs(dx) > abs(dy)) {
+            if (dx > 0) this.move(1, 0);
+            else this.move(-1, 0);
+        } else {
+            if (dy > 0) this.move(0, -1);
+            else this.move(0, 1);
+        }
+    }
+
+    this.moveBackward = () => {
+        let originalDir = this.dir;
+        this.dir += PI;
+        this.moveForward();
+        this.dir = originalDir;
+    }
+
+    this.moveRight = () => {
+        let originalDir = this.dir;
+        this.dir += (-PI / 2);
+        this.moveForward();
+        this.dir = originalDir;
+    }
+
+    this.moveLeft = () => {
+        let originalDir = this.dir;
+        this.dir += (PI / 2);
+        this.moveForward();
+        this.dir = originalDir;
     }
 
 }
