e7021ff44f19518a5796b935368e5be5583a7fa3
garganshul108 <sci.agarg@gmail.com>
2019-10-28 15:46:46 +0530

    documented

--- core/game.js
+++ core/game.js
@@ -1,17 +1,32 @@
-
-viewAngle = (3.0 / 4.0) * Math.PI;
 function Game(width, height, rows, cols) {
 
+    // dimensions of the map-display window
     this.width = width;
     this.height = height;
+
+    // dimensions of the map
     this.cols = cols;
     this.rows = rows;
+
     this.scaleX = this.width / this.cols;
     this.scaleY = this.height / this.rows;
+
+    // map
     this.design = [];
     this.player = undefined;
 
-    // initialization of the map
+    /**
+     * Initialization of the map
+     * 
+     * At the beginning the map doesn't contains
+     * any walls or boundaries
+     * 
+     * 
+     * Legends
+     * 0 => Empty Space
+     * 1 => Filled Space (Block/Wall)
+     * 
+     ***/
     for (let i = 0; i < this.rows; i++) {
         let row = [];
         for (let j = 0; j < this.cols; j++) {
@@ -25,13 +40,36 @@ function Game(width, height, rows, cols) {
         return 0;
     }
 
-
+    /**
+     * @function
+     * - Places block on the desired location
+     * - Contiguous Blocks represent a wall
+     * 
+     * @params (x, y, locations)
+     * x, y: coordinates of a loction
+     * locations: @array of @object {x, y}
+     * 
+     */
     this.place = (y, x, locations) => {
         if (!locations) this.design[y][x] = 1;
         else for (let v of locations) {
             this.design[v.y][v.x] = 1;
         }
     }
+
+
+    /**
+     * @function
+     * - Places contiguous block
+     *   from start to end
+     * 
+     * - The Line can only be
+     *   verticle or horizontal
+     * 
+     * @params (x1, y1, x2, y2)
+     * x1, y1 : starting point
+     * x2, y2 : ending points 
+     **/
 
     this.placeLine = (y1, x1, y2, x2) => {
         if (y1 == y2) {
@@ -45,6 +83,16 @@ function Game(width, height, rows, cols) {
         }
     }
 
+    /**
+     * @function
+     * - Places rectangular block
+     *   from top-left to bottom-right corners
+     *    
+     * @params (x1, y1, x2, y2)
+     * x1, y1 : corner 1
+     * x2, y2 : corner 2
+     *  
+     **/
     this.placeBlock = (y1, x1, y2, x2) => {
         for (let i = min(y1, y2); i <= max(y1, y2); i++) {
             for (let j = min(x1, x2); j <= max(x1, x2); j++) {
@@ -59,6 +107,7 @@ function Game(width, height, rows, cols) {
             this.design[v.y][v.x] = 0;
         }
     }
+
 
     this.unplaceBlock = (y1, x1, y2, x2) => {
         for (let i = min(y1, y2); i <= max(y1, y2); i++) {
@@ -76,16 +125,40 @@ function Game(width, height, rows, cols) {
     }
 
 
+    /***
+     * @function
+     * - adds the players to the game board
+     * 
+     * @params
+     * - object of the Player (./player.js)
+     *
+     */
+
     this.addPlayer = (player) => {
         this.player = player;
     }
 
+
+
+
+    /***
+     * @function
+     * - Draws the 2D grid of the map
+     * - Player loaction
+     * - FOV of the player
+     * 
+     * @params
+     * - topLeftX, topLeftY: top-left corner of the display window
+     */
+
     this.draw = (topLeftX, topLeftY) => {
+
+        // boundaries of the map
         stroke(255);
         fill(10);
         rect(topLeftX, topLeftY, this.width, this.height);
 
-        // drawing map
+        // drawing blocks
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
                 if (this.design[i][j] == 1) {
@@ -103,6 +176,8 @@ function Game(width, height, rows, cols) {
             rect(topLeftX + this.player.x * this.scaleX, topLeftY + this.player.y * this.scaleY, this.scaleX, this.scaleY);
             stroke(200, 200, 200, 15);
             // strokeWeight(2);
+
+            // drawing the FOV of the player
             for (let angle = this.player.dir - viewAngle / 2; angle <= this.player.dir + viewAngle / 2; angle += PI / 200) {
                 let r = 50;
                 line(topLeftX + this.player.x * this.scaleX + this.scaleX / 2,
@@ -112,10 +187,6 @@ function Game(width, height, rows, cols) {
             }
         }
 
-
-        // console.log("map is drawn");
     }
-
-
 
 }
